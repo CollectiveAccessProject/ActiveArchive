477844776ec074d1b4a9c77c1acdc04e3c3aa3e2
Seth <seth@collectiveaccess.org>
2016-10-13 11:27:47 -0400

    Change query

--- www/js/services.js
+++ www/js/services.js
@@ -46,7 +46,7 @@ angular.module('mfactivearchive.services', ['mfactivearchive.config'])
     	
 		var promise = $http({
 		method : 'POST',
-		url : dataConfig.backend + '/service.php/simple/exhibitions?q=ca_occurrences.event_dates:' + decade + '&limit=' + l + '&start=' + s + '&noCache=' + dataConfig.noCache
+		url : dataConfig.backend + '/service.php/simple/exhibitions?q=ca_occurrences.event_dates:"' + decade + '"&limit=' + l + '&start=' + s + '&noCache=' + dataConfig.noCache
 		}).then(function(response) {
 			$log.log('Load complete');
 			return response.data['data'];
@@ -74,7 +74,7 @@ angular.module('mfactivearchive.services', ['mfactivearchive.config'])
     	$log.log('loading on view');
     	var promise = $http({
 		method : 'POST',
-		url : dataConfig.backend + '/service.php/simple/exhibitions?q=ca_occurrences.event_dates:may 2015&noCache=' + dataConfig.noCache
+		url : dataConfig.backend + '/service.php/simple/exhibitions?q=ca_occurrences.event_dates:"may 2015"&noCache=' + dataConfig.noCache
 		}).then(function(response) {
 			$log.log('Load complete');
 			return response.data['data'];
